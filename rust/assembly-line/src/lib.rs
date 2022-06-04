a13ec8ef229b8a32708b8ccd2cf4fde75c347d23
Asif Iqbal Rahaman <asif256000@gmail.com>
2022-06-05 00:40:26 +0530

    :crab: :tada: Rust assembly-line solved

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -2,10 +2,18 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+const CAR_PRODUCTION_BASE_RATE: f64 = 221.0;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let success_rate = match speed {
+        0..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        _ => panic!("Illegal speed setting: {}", speed),
+    };
+    CAR_PRODUCTION_BASE_RATE * <f64>::from(speed) * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.).floor() as u32
 }
