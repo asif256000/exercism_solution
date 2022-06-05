b92d7ef414c24a4b23db05447420ad2f1a72128b
Asif Iqbal Rahaman <asif256000@gmail.com>
2022-06-05 17:52:25 +0530

    :crab: :tada: Rust magazine-cutout solved

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,22 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut note_map: HashMap<&str, u8> = HashMap::new();
+    
+    for note_word in note {
+        *note_map.entry(note_word).or_insert(0) += 1;
+    }
+
+    for mag_word in magazine {
+        if !note_map.contains_key(mag_word) {
+            continue;
+        }
+
+        match note_map.get_mut(mag_word).unwrap() {
+            0 => (),
+            w => *w -= 1,
+        }
+    }
+
+    note_map.values().sum::<u8>() == 0
 }
