2d9de8e9da7b25744b81b8c0bb1134370c0bf72f
asif256000 <asif256000@gmail.com>
2022-09-11 00:06:40 -0400

    ✂️ remove previous version js files

--- javascript/windowing-system/windowing-system.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-
-/**
- * Implement the classes etc. that are needed to solve the
- * exercise in this file. Do not forget to export the entities
- * you defined so they are available for the tests.
- */
