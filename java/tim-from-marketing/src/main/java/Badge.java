be0171ce4e19803b8b7ac74fc33dbed247d89b1a
asif256000 <asif256000@gmail.com>
2024-10-05 13:01:00 -0400

    Java Marketing ID solved

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,15 +1,8 @@
 class Badge {
     public String print(Integer id, String name, String department) {
-        String id_print = "";
-        if( id != null) {
-            id_print += "[" + id + "] - ";
-        }
-        id_print += name;
-        if( department != null) {
-            id_print += " - " + department.toUpperCase();
-        } else {
-            id_print += " - OWNER";
-        }
-        return id_print;
+        return String.format("%s%s - %s",
+                id != null ? String.format("[%d] - ", id) : "",
+                name,
+                department != null ? department.toUpperCase() : "OWNER");
     }
 }
