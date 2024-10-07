eb814f8a8e46689c5b91f1d0e2f7ddad0688b615
asif256000 <asif256000@gmail.com>
2024-10-07 00:53:17 -0400

    Java Appointment solved

---
+++ java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class AppointmentScheduler {
+    public LocalDateTime schedule(String appointmentDateDescription) {
+        DateTimeFormatter parser = DateTimeFormatter.ofPattern("M/d/yyyy HH:mm:ss");
+        return LocalDateTime.parse(appointmentDateDescription, parser);
+    }
+
+    public boolean hasPassed(LocalDateTime appointmentDate) {
+        LocalDateTime now = LocalDateTime.now();
+        return appointmentDate.isBefore(now);
+    }
+
+    public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
+        return appointmentDate.getHour() >= 12 && appointmentDate.getHour() < 18;
+    }
+
+    public String getDescription(LocalDateTime appointmentDate) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("h:mm a");
+        
+        String date = appointmentDate.format(dateFormatter);
+        String time = appointmentDate.format(timeFormatter);
+
+        return String.format("You have an appointment on %s, at %s.", date, time);
+    }
+
+    public LocalDate getAnniversaryDate() {
+        LocalDate today = LocalDate.now();
+        return today.withMonth(9).withDayOfMonth(15);
+    }
+}
