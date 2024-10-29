c606d4c1fdfb2d48907330240828c2320798f128
asif256000 <asif256000@gmail.com>
2024-10-29 16:15:31 -0500

    Java NFS Solved

---
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -0,0 +1,45 @@
+class NeedForSpeed {
+    private int speed;
+    private int batteryDrain;
+    private int distance = 0;
+    private int battery = 100;
+
+    NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+    }
+
+    public boolean batteryDrained() {
+        return this.battery < this.batteryDrain;
+    }
+
+    public int distanceDriven() {
+        return this.distance;
+    }
+
+    public void drive() {
+        if (this.batteryDrained()) {
+            return;
+        }
+        this.distance += this.speed;
+        this.battery -= this.batteryDrain;
+    }
+
+    public static NeedForSpeed nitro() {
+        return new NeedForSpeed(50, 4);
+    }
+}
+
+class RaceTrack {
+    private int distance;
+    RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean canFinishRace(NeedForSpeed car) {
+        while (car.distanceDriven() < this.distance && !car.batteryDrained()) {
+            car.drive();
+        }
+        return car.distanceDriven() >= this.distance;
+    }
+}
